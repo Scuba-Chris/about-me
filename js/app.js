403dc9729642f7ee1540242729747717b5fa512a
Scuba-Chris <chrisceder@hotmail.com>
2019-08-07 21:34:55 -0700

    added more questions, corrected stucture of the html

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 
 var answers = [];
-   
+
 var hobby = confirm('Is scuba diving the greatest hobby know to man? (y or n)');
 
 answers.push(hobby);
 if(hobby) {
-    alert('You are absolutely correct!!');
+  alert('You are absolutely correct!!');
 }
 else{
-    alert('You are WRONG!');
+  alert('You are WRONG!');
 }
 console.log(hobby);
 
@@ -17,10 +17,10 @@ var ocean = prompt('Is the Pacific Ocean the largest Ocean in the world?').toLow
 answers.push(ocean);
 
 if(ocean.startsWith('y')){
-    alert('You are correct!');
+  alert('You are correct!');
 }
 else{
-    alert('Try again bro!');
+  alert('Try again bro!');
 }
 console.log(ocean);
 
@@ -29,10 +29,10 @@ var fish = prompt('Is the Grunt Sculpin the coolest fish in the sea?').toLowerCa
 answers.push(fish);
 
 if(fish.startsWith('y')){
-    alert('HELLS to the Yeah!');
+  alert('HELLS to the Yeah!');
 }
 else{
-    alert('You need to go learn some fish!');
+  alert('You need to go learn some fish!');
 }
 console.log(fish);
 
@@ -41,26 +41,73 @@ var q4 = confirm('Am I an awsome scuba Diver Master?');
 answers.push(q4);
 
 if(q4){
-    alert('Absolutely!!');
+  alert('Absolutely!!');
 }
 else{
-    alert('Yea Yea you are WRONG!');
+  alert('Yea Yea you are WRONG!');
 }
 console.log(q4);
 
-var q5 = confirm('Is coding challeging but incredably fun?!')
+var q5 = confirm('Is coding challeging but incredably fun?!');
 
-answer.push(q5);
+answers.push(q5);
 
 if(q5){
-    alert('Correct! It most certainly is!!';)
+  alert('Correct! It most certainly is!!');
 }
 else{
-    alert('You should try it again!');
+  alert('You should try it again!');
 }
 console.log(q5);
 
+answers.push(cities);
+
+for(var i = 0; i <= 4; i++){
+  var cities = prompt('How many cities have I lived in?');
+  switch(cities.toLowerCase()){
+  case '1':
+    console.log('To low!');
+    alert('To low! Try again.');
+    break;
+
+  case '2':
+    console.log('To low!');
+    alert('To low! Try again.');
+    break;
+
+  case '3':
+    console.log('Correct');
+    alert('Correct!');
+    i = 4;
+    break;
+
+  case '4':
+    console.log('To high!');
+    alert('To high! Try again.');
+    break;
+
+  case 'deafult':
+    console.log('Not a number');
+    alert('Not a number from 1 to 6');
+    break;
+
+  }}
 
 
+var numbOfCountries = ['usa', 'canada', 'equador', 'mexico' ];
 
+for(var i = 0; i <= 5; i++){
+  var q7 = prompt('What countries have I been to?').toLowerCase();
+  if(numbOfCountries.includes(q7)){
+    console.log('Correct');
+    alert('You are correct!!');
+    alert('You got ' + answers.indexOf() + ' out of 7' );
+    numbOfCountries.remove(q7);
+    answers.push(q7);
+  }
+  else {
+    console.log('Wrong');
+    alert('Wrong try again!');
+  }
+}
 
