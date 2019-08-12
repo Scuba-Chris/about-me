c2999bcf10203e80f613552c673f857645e739f5
Scuba-Chris <chrisceder@hotmail.com>
2019-08-11 18:11:49 -0700

    added asking person for there name and sytax errors

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
+'use strict'
+
+var whatName = prompt('what is you name');
+alert(whatName + ' , thank you for coming to my page.');
+console.log(whatName);
 
 var answers = [];
-   
+
+//question 1
 var hobby = confirm('Is scuba diving the greatest hobby know to man? (y or n)');
 
 answers.push(hobby);
@@ -12,6 +18,7 @@ else{
 }
 console.log(hobby);
 
+//question 2
 var ocean = prompt('Is the Pacific Ocean the largest Ocean in the world?').toLowerCase();
 
 answers.push(ocean);
@@ -24,6 +31,7 @@ else{
 }
 console.log(ocean);
 
+//question 3
 var fish = prompt('Is the Grunt Sculpin the coolest fish in the sea?').toLowerCase();
 
 answers.push(fish);
@@ -36,6 +44,7 @@ else{
 }
 console.log(fish);
 
+//question 4
 var q4 = confirm('Am I an awsome scuba Diver Master?');
 
 answers.push(q4);
@@ -48,12 +57,13 @@ else{
 }
 console.log(q4);
 
+//question 5
 var q5 = confirm('Is coding challeging but incredably fun?!')
 
 answer.push(q5);
 
 if(q5){
-    alert('Correct! It most certainly is!!';)
+    alert('Correct! It most certainly is!!');
 }
 else{
     alert('You should try it again!');
